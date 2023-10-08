03a42b1c9f94f364784927ff925854b6be5f8e66
EL KAAMOUSE Issam <issamelkaamouse@gmail.com>
2023-10-08 23:28:20 +0100

    the delete function is set in the cart products page

--- cart.js
+++ cart.js
@@ -4,7 +4,12 @@ let cartCounter = localStorage.getItem("cartItemCount");
 document.querySelector(".cartCount").innerHTML = cartCounter;
 
 let cartProductsList = JSON.parse(localStorage.getItem("cartProductsList"));
+let totalPrice = 0;
+document.querySelector(".cartTotalPrice").innerHTML = totalPrice;
+
 renderCartProducts();
+getTotalPriceOfCartProducts5();
+
 function renderCartProducts() {
   let htmlProducts = ``;
   cartProductsList.forEach((product, index) => {
@@ -23,7 +28,7 @@ function renderCartProducts() {
             <button onclick="deleteCartProduct(${index})" class="cartDeleteBtn">Delete</button>
           </div>
           <div class="cartProdQuantity">
-            <i onclick="decrementQuantity(${index});" class="fa-solid fa-square-minus"></i>
+            <i onclick="decrementQuantity(${index});  decrementCartTotalPrice();" class="fa-solid fa-square-minus"></i>
             <span>${product.quantity}</span>
             <i onclick="incrementQuantity(${index});"  class="fa-solid fa-square-plus"></i>
           </div>
@@ -35,33 +40,37 @@ function renderCartProducts() {
   });
   document.querySelector(".cartDetails").innerHTML = htmlProducts;
 }
+
 function decrementQuantity(index) {
   if (cartProductsList[index].quantity > 1) {
     cartProductsList[index].quantity--;
+
+    cartProductsList[index].price -= updateProductPrice(index);
     localStorage.setItem("cartProductsList", JSON.stringify(cartProductsList));
+    getTotalPriceOfCartProducts1(index);
     renderCartProducts();
   }
 }
 function incrementQuantity(index) {
   cartProductsList[index].quantity++;
-  updateProductPrice(index);
+  cartProductsList[index].price =
+    updateProductPrice(index) * cartProductsList[index].quantity;
   localStorage.setItem("cartProductsList", JSON.stringify(cartProductsList));
+  getTotalPriceOfCartProducts(index);
   renderCartProducts();
 }
 
+// for select the initial price of the product not current price
 function updateProductPrice(index) {
+  let initialPrice = 0;
   JSON.parse(localStorage.getItem("productsList")).forEach((item) => {
     if (item.id === cartProductsList[index].id) {
-      let initialPrice = item.price;
-      cartProductsList[index].price =
-        initialPrice * cartProductsList[index].quantity;
+      initialPrice = item.price;
     }
   });
+  return initialPrice;
 }
 
-// document
-//   .querySelector(".cartDeleteBtn")
-//   .addEventListener("click", () => deleteCartProduct());
 let productsList = JSON.parse(localStorage.getItem("productsList"));
 
 function deleteCartProduct(index) {
@@ -70,10 +79,13 @@ function deleteCartProduct(index) {
   console.log(productsList);
   productsList[indexOfProductDeletedFromCart(index)].isAddedToCart = false;
   localStorage.setItem("productsList", JSON.stringify(productsList));
-  cartCounter--;
+  if (cartCounter >= 1) {
+    cartCounter--;
+  }
   localStorage.setItem("cartItemCount", cartCounter);
   document.querySelector(".cartCount").innerHTML = cartCounter;
   localStorage.setItem("cartProductsList", JSON.stringify(cartProductsList));
+  deletePrice(index);
   renderCartProducts();
 }
 
@@ -85,4 +97,39 @@ function indexOfProductDeletedFromCart(ind) {
     }
   });
   return i;
+}
+
+// this function calculate the total price in the cart
+// document.querySelector(".cartTotalPrice").innerHTML = `${totalPrice} $`;
+// cartTotalPrice();
+// let totalCartPrice = 0;
+// function cartTotalPrice() {
+//   totalCartPrice = totalCartPrice + getTotalPricesOfCartProducts();
+//   document.querySelector(".cartTotalPrice").innerHTML = `${totalCartPrice} $`;
+// }
+
+// function decrementCartTotalPrice() {
+//   console.log("gggg");
+// }
+
+function getTotalPriceOfCartProducts5() {
+  cartProductsList.forEach((item) => {
+    totalPrice += item.price;
+  });
+  document.querySelector(".cartTotalPrice").innerHTML = totalPrice;
+  return totalPrice;
+}
+
+function deletePrice(index) {
+  totalPrice = totalPrice - cartProductsList[index].price;
+  document.querySelector(".cartTotalPrice").innerHTML = totalPrice;
+}
+
+function getTotalPriceOfCartProducts(index) {
+  totalPrice += updateProductPrice(index);
+  document.querySelector(".cartTotalPrice").innerHTML = totalPrice;
+}
+function getTotalPriceOfCartProducts1(index) {
+  totalPrice -= updateProductPrice(index);
+  document.querySelector(".cartTotalPrice").innerHTML = totalPrice;
 }
